5340e9b2c9accb7b0ae6c1ab411b3740d2f13424
zawminhtutx <zawminhtutx@gmail.com>
2024-09-19 14:55:09 +0630

    add orders fetching apporach 2

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -5,47 +5,35 @@ import 'package:shop_app/widgets/app_drawer.dart';
 import '../provider/orders.dart';
 import '../widgets/order_item.dart' as ui;
 
-class OrderScreen extends StatefulWidget {
+class OrderScreen extends StatelessWidget {
   const OrderScreen({
     super.key,
   });
 
   @override
-  State<OrderScreen> createState() => _OrderScreenState();
-}
-
-class _OrderScreenState extends State<OrderScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    setState(() {
-      _isLoading = true;
-    });
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderContainer = Provider.of<Orders>(context);
+    final orderContainer = Provider.of<Orders>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         title: const Text('My Orders'),
       ),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
+            );
+          } else {
+            return ListView.builder(
               itemCount: orderContainer.items.length,
               itemBuilder: (ctx, index) =>
-                  ui.OrderItem(orderContainer.items[index])),
+                  ui.OrderItem(orderContainer.items[index]),
+            );
+          }
+        },
+      ),
       drawer: const AppDrawer(),
     );
   }
